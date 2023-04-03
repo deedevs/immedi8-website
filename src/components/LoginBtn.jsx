1c728e6195c99039d138b4b834ec01cf3af18cc6
Peter Junior Mendy <petermendy5@gmail.com>
2023-04-03 13:43:43 +0000

    update faq, login button

--- src/components/LoginBtn.jsx
+++ src/components/LoginBtn.jsx
@@ -2,11 +2,15 @@ import React from "react";
 
 const LoginBtn = ({ className }) => {
   return (
-    <button
-      className={`py-3 px-10 bg-green-600 border text-lg text-white hover:bg-white hover:text-black hover:border hover:border-red-600 font-semibold rounded-full ${className}`}
-    >
-      Login
-    </button>
+    <>
+      <a
+        href="https:imoneycli.immedi8money.com"
+        target="_blank"
+        className={`py-3 px-10 bg-green-600 border text-lg text-white hover:bg-white hover:text-black hover:border hover:border-red-600 font-semibold rounded-full ${className}`}
+      >
+        <button>Login</button>
+      </a>
+    </>
   );
 };
 
