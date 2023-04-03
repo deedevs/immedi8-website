99b1e454db7cf6f53f31cccf5ab0fe61350cd0c6
David Ladipo <deedevs21@gmail.com>
2023-04-03 14:43:56 +0000

    event

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,6 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes";
 import App from "./App";
 import "./index.css";
-import ReactGA from "react-ga";
-
-const TRACKING_ID = "G-JTTSGCCD1B";
-
-ReactGA.initialize(TRACKING_ID);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
