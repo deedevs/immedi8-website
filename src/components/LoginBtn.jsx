dee902218ffcbdaa5e00b1b7c392292d1fb7856d
Peter Junior Mendy <petermendy5@gmail.com>
2023-04-06 16:24:49 +0000

    update href for the login button

--- src/components/LoginBtn.jsx
+++ src/components/LoginBtn.jsx
@@ -4,7 +4,7 @@ const LoginBtn = ({ className }) => {
   return (
     <>
       <a
-        href="https:imoneycli.immedi8money.com"
+        href="https://imoneycli.immedi8money.com"
         target="_blank"
         className={`py-3 px-10 bg-green-600 border text-lg text-white hover:bg-white hover:text-black hover:border hover:border-red-600 font-semibold rounded-full ${className}`}
       >
