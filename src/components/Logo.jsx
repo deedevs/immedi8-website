841b3220c977505c6c7fc61713b745d6d2373b1b
David Ladipo <deedevs21@gmail.com>
2023-05-03 11:19:50 +0000

    logo update

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../assets/logo.png";
+import logo from "../assets/imoneyNew.png";
 import logo2 from "../assets/logo2.png";
 import logo3 from "../assets/logo3.png";
 
